a6ef62815410184478a312c6fb2c8c567f932cf6
Clemens Buchacher <drizzd@aon.at>
2013-04-07 15:40:18 +0200

    add debugging flag

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-gcc -fvisibility=hidden -fpic -c -olibsystem1.o libsystem1.c
-gcc -fvisibility=hidden -fpic -c -osubsystem1.o subsystem1.c
-gcc -fvisibility=hidden -fpic -c -olibsystem2.o libsystem2.c
-gcc -fvisibility=hidden -fpic -c -osubsystem2.o subsystem2.c
-gcc -shared -osubsystem1.so subsystem1.o libsystem1.o
-gcc -shared -osubsystem2.so subsystem2.o libsystem2.o
-gcc main.c subsystem1.so subsystem2.so -osystem
+CFLAGS="-fvisibility=hidden -Wall -g"
+
+gcc $CFLAGS -fpic -c -olibsystem1.o libsystem1.c
+gcc $CFLAGS -fpic -c -osubsystem1.o subsystem1.c
+gcc $CFLAGS -fpic -c -olibsystem2.o libsystem2.c
+gcc $CFLAGS -fpic -c -osubsystem2.o subsystem2.c
+gcc $CFLAGS -shared -osubsystem1.so subsystem1.o libsystem1.o
+gcc $CFLAGS -shared -osubsystem2.so subsystem2.o libsystem2.o
+gcc $CFLAGS main.c subsystem1.so subsystem2.so -osystem
